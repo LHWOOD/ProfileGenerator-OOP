4fc067e07b5ca30c5f2489fc37a79bb8bae4e641
LHWOOD <hutsonwood@gmail.com>
2021-05-17 22:18:28 -0600

    began testing functions in employee.test file

--- __test__/Employee.test.js
+++ __test__/Employee.test.js
@@ -1,3 +1,8 @@
 const Employee = require("../lib/Employee");
 
-describe("employee", () => {});
+describe("employee", () => {
+  it("should create an employee object", () => {
+    const employee = new Employee();
+    expect(typeof employee).toBe("object");
+  });
+});
